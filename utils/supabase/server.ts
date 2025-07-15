52cf627372e58e6c290271a0f900de874c98efd6
pmazumder3927 <pmazumder3927@gmail.com>
2025-07-14 18:31:26 -0700

    fixed supabase server client

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,30 +2,23 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export async function createClient() {
-  const cookieStore = await cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
+        async getAll() {
+          const cookieStore = await cookies();
+          return cookieStore.getAll();
         },
-        set(name: string, value: string, options: CookieOptions) {
+        async setAll(cookiesToSet) {
           try {
-            cookieStore.set({ name, value, ...options });
+            const cookieStore = await cookies();
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
           } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value: "", ...options });
-          } catch (error) {
-            // The `remove` method was called from a Server Component.
+            // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
